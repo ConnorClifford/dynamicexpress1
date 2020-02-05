675487c567f76975ee51073bb257e2f92bff25f5
Connor <connorclifford17@gmail.com>
2020-02-05 18:42:27 -0500

    person object separate from server file

--- index.js
+++ index.js
@@ -2,19 +2,8 @@ const Express = require('express');
 const App = Express();
 const port = 80;
 
-const Person = require("./Person.js");
-let p = new Person("Connor", "blue");
-let people = [];
-
-const faker = require('faker');
-
-for(let i=0;i<99;i++){
-    let name = faker.name.findName();
-    let color = faker.commerce.color();
-    people.push(new Person(name, color));
-}
-
-//console.log(people[10]);
+const People = require("./people.js")
+let people = new People();
 
 App.get("/person/name/:name", (req, res) => {
 
@@ -31,23 +20,8 @@ App.get("/person/name/:name", (req, res) => {
 
 App.get("/person/color/:color", (req, res) => {
 
-    let result = {"error": "Nothing found that matches that. :("};
+    res.send(people.readColor(req));  
 
-    people.forEach((value) => {
-        if(value.color == req.params.color){
-            result = value;
-        }
-    });
-
-    if(result.error){
-        //in red
-        console.log(req.path);
-    }
-    else{
-        //in green
-        console.log(req.path);
-    }
-    res.send(result);
 });
 
 App.listen(port, () =>{
---
+++ people.js
@@ -0,0 +1,39 @@
+const Person = require("./Person.js");
+const faker = require('faker');
+
+class People {
+
+    constructor(){   
+        this.people = [];
+
+        for(let i=0; i<99; i++){
+            let name = faker.name.findName();
+            let color = faker.commerce.color();
+            this.people.push(new Person(name, color));
+        }
+
+    }
+
+    readColor(req){
+        let result = {"error": "Nothing found that matches that. :("};
+
+        this.people.forEach((value) => {
+            if(value.color == req.params.color){
+                result = value;
+            }
+        });
+    
+        if(result.error){
+            //in red
+            console.log(req.path);
+        }
+        else{
+            //in green
+            console.log(req.path);
+        }
+        return result;
+    }
+
+}
+
+module.exports = People;
